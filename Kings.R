3a40113064964ed3f72d58ebaa20f50b1d428a80
Magnusiversen <73470021+Magnusiversen@users.noreply.github.com>
2021-01-06 16:04:07 +0100

    Final Project

---
+++ Kings.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+data_til_digital_metode <- data_til_digital_metode %>%
+  mutate(duration = End_date-Start_date, midyear = (End_date+Start_date)/2)
+data_til_digital_metode %>%
+  ggplot(aes(x = midyear, y = duration)) + geom_point() +
+  geom_smooth(mapping = aes(x = midyear, y = duration), method=lm) +
+  labs(title = "How long danish kings ruled over time",
+                                                                                                                                  x = "Year ",
+                                                                                                                                  y = "Year they ruled") + theme_bw() + theme(text = element_text(size = 14))
+
+
